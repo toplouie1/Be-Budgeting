fd314b45838d687879d41d473a6262b7d64ae279
Nima sherpa <Toplouie78@gmail.com>
2022-01-18 11:53:17 -0500

    makde routes to all and index

--- app.js
+++ app.js
@@ -9,8 +9,20 @@ app.use(express.json());
 
 const transaction = require("./models/Transaction.js");
 
+// The Home page
 app.get("/", (req, res) => {
 	res.send("welcome to The Transaction Back");
+});
+
+// The transaction Page
+app.get("/transaction", (req, res) => {
+	res.send(transaction);
+});
+
+// sending the preticular index
+app.get("/transaction/:index", (req, res) => {
+	const { index } = req.params;
+	transaction[index] ? res.send(transaction[index]) : res.redirect(404);
 });
 
 module.exports = app;
