cba54039b5926ce213bbb64e8a5cff64065e6d82
Nima sherpa <Toplouie78@gmail.com>
2022-01-21 16:49:36 -0500

    push

--- app.js
+++ app.js
@@ -7,54 +7,54 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-const transaction = require("./models/Transaction.js");
+const transactions = require("./models/Transaction.js");
 
 // The Home page
 app.get("/", (req, res) => {
 	res.send("welcome to The Transaction Back");
 });
 
-// The transaction Page
-app.get("/transaction", (req, res) => {
-	res.send(transaction);
+// All Transacrtion information
+app.get("/transactions", (req, res) => {
+	res.json(transactions);
 });
 
 // sending the preticular index
-app.get("/transaction/:index", (req, res) => {
+app.get("/transactions/:index", (req, res) => {
 	const { index } = req.params;
-	transaction[index] ? res.send(transaction[index]) : res.redirect(404);
+	transactions[index] ? res.send(transactions[index]) : res.redirect(404);
 });
 
 // creating a post adding a data at the end
-app.post("/transaction", (req, res) => {
-	transaction.push(req.body);
-	res.json(transaction[transaction.length - 1]);
+app.post("/transactions", (req, res) => {
+	transactions.push(req.body);
+	res.json(transactions[transactions.length - 1]);
 });
 
 // to delete something from a perticular index
-app.delete("/transaction/:index", (req, res) => {
+app.delete("/transactions/:index", (req, res) => {
 	const { index } = req.params;
-	res.json(transaction.splice(index, 1));
+	res.json(transactions.splice(index, 1));
 });
 
 // to edit something from your list
-app.put("/transaction/:index", (req, res) => {
+app.put("/transactions/:index", (req, res) => {
 	const { index } = req.params;
 	// guard clause
-	if (!transaction[index]) {
+	if (!transactions[index]) {
 		res.status(422).json({ error: "Not found" });
 		return;
 	}
 	// we are destructuring the req that is coming ..
 	const { date, name, amount, from } = req.body;
 	if (date && name && from && amount) {
-		transaction[index] = {
+		transactions[index] = {
 			date,
 			name,
 			amount,
 			from,
 		};
-		res.json(transaction[index]);
+		res.json(transactions[index]);
 	} else {
 		res.status(422).json({
 			error: "Invalid inputs",
--- models/Transaction.js
+++ models/Transaction.js
@@ -1,14 +1,44 @@
 module.exports = [
 	{
 		date: "April 1",
-		name: "Top",
-		amount: 800,
-		from: "Work",
+		name: "Groceries",
+		amount: -30,
+		from: "Ideal",
 	},
 	{
-		date: "january",
-		name: "Bottom",
-		amount: -100,
-		from: "Home",
+		date: "April 1",
+		name: "Shopping",
+		amount: -47,
+		from: "Target",
+	},
+	{
+		date: "April 2",
+		name: "Income",
+		amount: 2000,
+		from: "Google",
+	},
+	{
+		date: "April 5",
+		name: "Gas",
+		amount: -25,
+		from: "Shell Gas Station",
+	},
+	{
+		date: "April 6",
+		name: "Rent",
+		amount: -700,
+		from: "Pike International, LLC",
+	},
+	{
+		date: "April 8",
+		name: "Income",
+		amount: 2000,
+		from: "Google",
+	},
+	{
+		date: "April 8",
+		name: "Groceries",
+		amount: -40,
+		from: "Mr. Kiwi",
 	},
 ];
